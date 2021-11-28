c1f0375e8f2ea34a42b11409952ffe9014dad2fd
sukdong-kim <73944038+sukdong-kim@users.noreply.github.com>
2021-11-28 17:05:53 +0900

    use table

---
+++ tablefx01/src/application/SampleController.java
@@ -0,0 +1,71 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class SampleController {
+	Connection conn;
+	Statement stmt = null;
+	ResultSet srs;
+	
+	ObservableList<Student> list;
+    @FXML
+    private TableView<Student> tableContent;
+    @FXML
+    private TableColumn<Student, String> col_email;
+
+    @FXML
+    private TableColumn<Student, String> col_id;
+
+    @FXML
+    private TableColumn<Student, String> col_name;
+
+    @FXML
+    private TableColumn<Student, String> col_phone;
+	
+	@FXML
+	private void initialize() {
+		conn = mysqlconnect.ConnectDb();
+		tablelookup();
+	}
+	public void tablelookup() {
+		
+		try {
+			stmt = conn.createStatement();
+			srs = stmt.executeQuery("select * from student");
+
+			list = FXCollections.observableArrayList();
+			while(srs.next()) {
+
+				String r1 = srs.getString("id");
+				String r2 = srs.getString("name");
+				String r3 = srs.getString("email");
+				String r4 = srs.getString("phone");				
+
+				Student student = new Student(r1,r2,r3,r4);	
+
+				list.add(student);
+
+				col_id.setCellValueFactory(new PropertyValueFactory<Student,String>("id"));
+				col_name.setCellValueFactory(new PropertyValueFactory<Student,String>("name"));
+				col_email.setCellValueFactory(new PropertyValueFactory<Student,String>("email"));
+				col_phone.setCellValueFactory(new PropertyValueFactory<Student,String>("phone"));										
+ 
+				tableContent.setItems(list);				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
