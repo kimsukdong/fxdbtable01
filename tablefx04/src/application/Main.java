1b0aacfc9c9b0f60da4502c5f3af076335edc175
kimsukdong <sdk1005@naver.com>
2021-11-29 15:40:02 +0900

    add insert

---
+++ tablefx04/src/application/Main.java
@@ -0,0 +1,29 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+// update db
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root =FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root,600,450);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Image image = new Image(getClass().getResource("/images/address.jpg").toExternalForm());
+			primaryStage.getIcons().add(image);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
